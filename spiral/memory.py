296987b09e61cce6b598cc76fd3ccf53aa7ba344
Luke Cordova <lukethegreat000@gmail.com>
2023-04-13 17:42:54 -0700

    memory class query method

--- spiral/memory.py
+++ spiral/memory.py
@@ -1,4 +1,7 @@
 import faiss
+import numpy as np
+import pandas as pd
+import ast
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
 
@@ -14,4 +17,30 @@ class Memory:
         :param k: Number of results to return.
         :return: Memory obtained from external memories.
         """
-        raise NotImplementedError("")
+
+        # get embedding of query
+        xQuery = np.array([get_embedding(query, engine="text-embedding-ada-002")])
+
+        # get vectors from memory 
+        df = pd.read_csv("data/memory.csv", index_col=0)
+        vectors = []
+
+        for val in df[["embedding"]].values:
+            vectors.append(ast.literal_eval(val[0]))
+
+        vectors = np.array(vectors)
+
+        # get faiss index to utilize search
+        _, d = vectors.shape
+        index = faiss.IndexFlatL2(d)
+        index.add(vectors)
+
+        # search for the indexes with similar embeddings
+        D, similarIndexes = index.search(xQuery, k)
+
+        # get the memory values at the found indexes
+        memoryValues = []
+        for i in similarIndexes[0]:
+            memoryValues.append(df[["combined"]].values[i])
+        return [memoryValues , D]
+        
